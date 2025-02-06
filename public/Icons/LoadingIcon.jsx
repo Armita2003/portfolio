b288d50adccd0d4b11192b0cb47258d5fadd4fad
Armita2oo3 <armita.mir2003@gmail.com>
2025-02-06 15:24:55 +0330

    added icons,fonts and images and removed default images

---
+++ public/Icons/LoadingIcon.jsx
@@ -0,0 +1,21 @@
+export function LoadingIcon() {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                width: "100%",
+            }}
+        >
+            <img
+                style={{
+                    width: "40%",
+                    height: "40%",
+                }}
+                src="https://s3-alpha-sig.figma.com/img/e17c/7e77/07b8ac2893283d23a9b18b1ecfb3faed?Expires=1739750400&amp;Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&amp;Signature=gRnj8~gOaRhaj0yDAcX0hc9BTWNGWHYS3EepYwHWlWvmoWlTvftj4WC2GQLmBoc~xSY8OWUfy9GV9VYidWvrHX9-HQbwCARyMqi29od~oM48S4LgY~H3UICdR6Qmf2s3TepDsY4Y24YFdgIU2njCSh83aJFL-BcmkieTDKJnW7-EtCl~2d2qBFmY7brGyciH9jLM03yjKZsqbv1GdON~p~UGR7qvRVaBkm-GuFEZPUFSESyqo6I35cdKOblbq6NUawJHeCJQIDgIaJTRImEJIA43sply-RU7-zc7c2pHMnNgsYT6j4rpu5vfKj5sOcliVaKd1wOrze5tf74w82pGVQ__"
+            />
+        </div>
+    );
+}
