6eeb5cc00a3fa7465c11b40d559d303454b79683
Armita2oo3 <armita.mir2003@gmail.com>
2025-02-07 19:02:37 +0330

    added fonts and icon and title for the website

--- pages/_app.js
+++ pages/_app.js
@@ -18,14 +18,23 @@ const ppHatton = localFont({
     variable: "--font-pphatton",
     display: "swap",
 });
-
+const satoshi = localFont({
+    src: [
+        {
+            path: "../public/fonts/Satoshi/Satoshi-Bold.woff",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    variable: "--font-satoshi",
+    display: "swap",
+});
 export default function App({ Component, pageProps }) {
     const getLayout = Component.getLayout ?? ((page) => page);
 
-    return <main className={ppHatton.variable}>{getLayout(<Component {...pageProps} />)}</main>;
-    // return (
-    //     <main className={ppHatton.variable}>
-    //         <Component {...pageProps} />
-    //     </main>
-    // );
+    return (
+        <>
+            <main className={`${ppHatton.variable} ${satoshi.variable}`}>{getLayout(<Component {...pageProps} />)}</main>
+        </>
+    );
 }
